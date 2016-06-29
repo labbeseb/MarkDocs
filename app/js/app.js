f2d7958a18aed445887fec6901503f00af0f533f
labbe.seb@gmail.com <x49tk8175>
2016-06-29 19:10:33 +0200

    mise en place babel/sass

---
+++ app/js/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var test = 'je suis un test',
+    bla = 'blabla';
+
+var msg = 'je suis un message de texte';
+
+// this is a comment
+// and another !
---
+++ src/scripts/sample.js
@@ -0,0 +1,7 @@
+const test = 'je suis un test',
+	bla = 'blabla';
+
+let msg = 'je suis un message de texte'
+
+// this is a comment
+// and another !a
