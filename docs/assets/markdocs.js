110edc7a84f2a901fc39e94aec90cf65d619a70b
Sebasiten <sebastien@audivox.fr>
2016-08-18 18:14:23 +0200

    réorganisation méthodes Markdocs

--- docs/assets/markdocs.js
+++ docs/assets/markdocs.js
@@ -12617,6 +12617,7 @@ var _createClass = function () { function defineProperties(target, props) { for 
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+//TODO : finir de commenter les methodes
 //TODO : faire la doc de markdocs...
 //TODO : page d'erreur si .md pas trouvé
 //TODO : trier liste filesNav
@@ -12625,37 +12626,149 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 var $ = jQuery,
     sdn = showdown;
 
-var GenericNames = {
-    docBody: 'markdocs-render',
-    docNav: 'markdocs-nav',
-    filesNav: 'markdocs-nav-files',
-    loadRender: 'markdocs-renderLoad',
-    data_btnFilesNav: 'file-name'
-},
-    ShowdownOptions = {
-    'tables': true
-},
-    DefaultOptions = {};
-
-var DocFiles = []; // tableau d'objet qui liste les différents fichiers de doc et leurs infos
-
+var Defaults = {
+    mdFiles: undefined,
+    showdownOptions: {
+        'tables': true
+    },
+    genericNames: {
+        container_docBody: 'markdocs-render',
+        container_docNav: 'markdocs-nav',
+        container_filesNav: 'markdocs-nav-files',
+        container_loadRender: 'markdocs-renderLoad',
+        data_btnFilesNav: 'file-name'
+    }
+};
 
 var Markdocs = function () {
     function Markdocs(options) {
         _classCallCheck(this, Markdocs);
 
+        var th = this;
+
         // =*= Propriétés
-        this._options = {};
-        this._options.filesArray = options.mdFiles;
+        th._settings = Markdocs._mergeOptions(options);
+        th._docFiles = [];
 
         // =*= Vérifications
-        if (typeof this._options.filesArray === 'undefined' || !$.isArray(this._options.filesArray)) throw new Error('Il manque l\'index mdFiles, mdFiles doit être un tableau...');
+        if (typeof th._settings.mdFiles === 'undefined' || !$.isArray(th._settings.mdFiles)) throw new Error('Il manque l\'index mdFiles, mdFiles doit être un tableau...');
 
         // =*= Execution
-        Markdocs._initPage(this._options.filesArray, this._options.filesArray[0]);
+        th._init();
+
+        // =*= Events
+        $('body').on('click', 'button', function () {
+            th._loadPage(th._settings.mdFiles, $(this).data(th._settings.genericNames.data_btnFilesNav));
+        });
     }
 
-    _createClass(Markdocs, null, [{
+    /**
+     * Chargement de la page avec le fichier par défaut à l'arrivée sur la doc
+     *
+     * @private
+     */
+
+
+    _createClass(Markdocs, [{
+        key: '_init',
+        value: function _init() {
+            this._loadPage(this._settings.mdFiles, this._settings.mdFiles[0]);
+        }
+
+        /**
+         * Merge les options user/default et retourne l'objet correspondant
+         *
+         * @private
+         */
+
+    }, {
+        key: '_loadPage',
+
+
+        /**
+         * Chargement ou rechargement de la page en fonction de la catégorie choisie
+         *
+         * @param arrayFiles
+         * @param filePage
+         * @private
+         */
+        value: function _loadPage(arrayFiles, filePage) {
+            var _this = this;
+
+            // -**- Lit le fichier md, le converti en html et envoie son contenu à la vue
+            Markdocs._readMdFile(filePage, function (data) {
+
+                _this._sendToHtml(_this._parseMdToHtml(data));
+            });
+
+            // -**- attend la fin du chargement du contenu pour générer la navigation...
+            $('#' + this._settings.genericNames.loadRender).ready(function () {
+                setTimeout(function () {
+                    // -***-
+                    _this._createNavPage();
+
+                    Markdocs._convertToObject(arrayFiles, _this._docFiles);
+
+                    if (arrayFiles.length > 1) {
+                        _this._createNavFiles();
+                    }
+                }, 100);
+            });
+        }
+    }, {
+        key: '_sendToHtml',
+        value: function _sendToHtml(txt) {
+            $('#' + this._settings.genericNames.container_docBody).html('<div id="' + this._settings.genericNames.container_loadRender + '">' + txt + '</div>');
+        }
+    }, {
+        key: '_parseMdToHtml',
+        value: function _parseMdToHtml(md) {
+            var converter = new sdn.Converter();
+
+            // application des options
+            for (var opp in this._settings.showdownOptions) {
+                converter.setOption(opp, this._settings.showdownOptions[opp]);
+            }
+
+            return converter.makeHtml(md);
+        }
+    }, {
+        key: '_createNavFiles',
+        value: function _createNavFiles() {
+            var th = this;
+
+            // création du container
+            var template = '<hr><div id="' + th._settings.genericNames.container_filesNav + '"><p>Catégories disponibles&nbsp;:</p></div>';
+
+            $('#' + th._settings.genericNames.container_docNav).append(template);
+
+            // parcourt du tableau de fichiers
+            th._docFiles.forEach(function (obj) {
+                $('#' + th._settings.genericNames.container_filesNav).append('<span>\n                        <button data-' + th._settings.genericNames.data_btnFilesNav + '="' + obj['path'] + '" class="btn__navfiles">' + obj['name'] + '</button>\n                    </span>');
+            });
+        }
+    }, {
+        key: '_createNavPage',
+        value: function _createNavPage() {
+            var th = this;
+
+            var titleList = $('h1, h2, h3, h4, h5, h6'),
+                template = '<nav><ul></ul></nav>';
+
+            $('#' + th._settings.genericNames.container_docNav).html(template);
+
+            titleList.each(function () {
+                var classTitle = 'title_nav title_' + $(this).get(0).tagName;
+
+                $('#' + th._settings.genericNames.container_docNav).find('ul').append('<li><a class="' + classTitle + '" href="#' + $(this).attr('id') + '">' + $(this).html() + '</a></li>');
+            });
+        }
+    }], [{
+        key: '_mergeOptions',
+        value: function _mergeOptions(options) {
+            return $.extend(true, {}, Defaults, options);
+        }
+    }, {
         key: '_rmExtension',
         value: function _rmExtension(file) {
 
@@ -12669,19 +12782,13 @@ var Markdocs = function () {
         }
     }, {
         key: '_convertToObject',
-        value: function _convertToObject(array) {
+        value: function _convertToObject(array, obj) {
 
             for (var i = 0; array.length > i; i++) {
-                DocFiles[i] = {};
-                DocFiles[i].name = this._rmExtension(array[i]);
-                DocFiles[i].path = array[i];
+                obj[i] = {};
+                obj[i].name = Markdocs._rmExtension(array[i]);
+                obj[i].path = array[i];
             }
-        }
-    }, {
-        key: '_sendToHtml',
-        value: function _sendToHtml(txt) {
-
-            $('#' + GenericNames.docBody).html('<div id="' + GenericNames.loadRender + '">' + txt + '</div>');
         }
     }, {
         key: '_readMdFile',
@@ -12708,197 +12815,7 @@ var Markdocs = function () {
                 throw new Error('Il y a eu une erreur lors du chargement du fichier...');
             }
         }
-    }, {
-        key: '_parseMdToHtml',
-        value: function _parseMdToHtml(md) {
-            var converter = new sdn.Converter();
-
-            // application des options
-            for (var opp in ShowdownOptions) {
-                converter.setOption(opp, ShowdownOptions[opp]);
-            }
-
-            return converter.makeHtml(md);
-        }
-    }, {
-        key: '_createNavFiles',
-        value: function _createNavFiles() {
-            // création du container
-            var template = '<hr><div id="' + GenericNames.filesNav + '"><p>Catégories disponibles&nbsp;:</p></div>';
-
-            $('#' + GenericNames.docNav).append(template);
-
-            // parcourt du tableau de fichiers
-            DocFiles.forEach(function (obj) {
-                $('#' + GenericNames.filesNav).append('<span>\n                        <button data-' + GenericNames.data_btnFilesNav + '="' + obj['path'] + '" class="btn__navfiles">' + obj['name'] + '</button>\n                    </span>');
-            });
-        }
-    }, {
-        key: '_createNavPage',
-        value: function _createNavPage() {
-            var titleList = $('h1, h2, h3, h4, h5, h6'),
-                template = '<nav><ul></ul></nav>';
-
-            $('#' + GenericNames.docNav).html(template);
-
-            titleList.each(function () {
-                var classTitle = 'title_nav title_' + $(this).get(0).tagName;
-
-                $('#' + GenericNames.docNav).find('ul').append('<li><a class="' + classTitle + '" href="#' + $(this).attr('id') + '">' + $(this).html() + '</a></li>');
-            });
-        }
-    }, {
-        key: '_initPage',
-        value: function _initPage(arrayFiles, filePage) {
-
-            // -**- Lit le fichier md, le converti en html et envoie son contenu à la vue
-            Markdocs._readMdFile(filePage, function (data) {
-
-                Markdocs._sendToHtml(Markdocs._parseMdToHtml(data));
-            });
-
-            // -**- attend la fin du chargement du contenu pour générer la navigation...
-            $('#' + GenericNames.loadRender).ready(function () {
-                setTimeout(function () {
-                    // -***-
-                    Markdocs._createNavPage();
-
-                    Markdocs._convertToObject(arrayFiles);
-                    console.log(arrayFiles);
-
-                    if (arrayFiles.length > 1) {
-                        Markdocs._createNavFiles();
-                    }
-                }, 100);
-            });
-        }
     }]);
 
     return Markdocs;
 }();
-
-/*
-
-
- function rmExtension(file){
-
- let arr = file.trim().split('.');
- arr.pop();
-
- let stringPath = arr.join('.'),
- resultNameArr = stringPath.split('/');
-
- return resultNameArr.pop();
- }
-
- function convertToObject(array){
-
- for(let i=0; array.length > i; i++){
- DocFiles[i] = {};
- DocFiles[i].name = rmExtension(array[i]);
- DocFiles[i].path = array[i];
- }
- }
-
- function sendToHtml(txt){
-
- $(`#${GenericNames.docBody}`).html(`<div id="${GenericNames.loadRender}">${txt}</div>`);
-
- }
-
- function readMdFile(urlFile, action){
-
- let reader = new XMLHttpRequest();
-
- reader.onload = function(){
- let data = this.responseText;
-
- if(typeof action === 'function') {
- action(data);
- }
- };
-
- reader.open("GET", urlFile + ((/\?/).test(urlFile) ? "&" : "?") + (new Date()).getTime(), true);
-
- reader.overrideMimeType("text/markdown; charset=UTF-8");
- reader.setRequestHeader("Cache-Control", "no-cache");
-
- if(reader.status == 0 || reader.status == 200) {
- return reader.send();
- }else{
- throw new Error('Il y a eu une erreur lors du chargement du fichier...');
- }
- }
-
- function parseMdToHtml(md){
- let converter = new showdown.Converter();
-
- // application des options
- for (let opp in ShowdownOptions){
- converter.setOption(opp, ShowdownOptions[opp]);
- }
-
- return converter.makeHtml(md);
-
- }
-
- function createNavFiles(){
- // création du container
- let template = `<hr><div id="${GenericNames.filesNav}"><p>Catégories disponibles&nbsp;:</p></div>`;
- $(`#${GenericNames.docNav}`).append(template);
-
- // parcourt du tableau de fichiers
- DocFiles.forEach(function(obj){
- $(`#${GenericNames.filesNav}`)
- .append(
- `<span>
- <button data-${GenericNames.data_btnFilesNav}="${obj['path']}" class="btn__navfiles">${obj['name']}</button>
- </span>`);
- });
-
- }
-
- function createNavPage(){
- let titleList = $('h1, h2, h3, h4, h5, h6'),
- template = `<nav><ul></ul></nav>`;
-
- $(`#${GenericNames.docNav}`).html(template);
-
- titleList.each( function(){
- let classTitle = `title_nav title_${ $(this).get(0).tagName }`;
-
- $(`#${GenericNames.docNav}`).find('ul').append(`<li><a class="${classTitle}" href="#${ $(this).attr('id') }">${ $(this).html() }</a></li>`)
- });
- }
-
- function initPage(filePage){
- readMdFile(filePage, data => {
- sendToHtml(parseMdToHtml(data));
- });
-
- $(`#${GenericNames.loadRender}`).ready( () =>{
- setTimeout( () => {
- createNavPage();
-
- convertToObject(MdFiles);
-
- if(MdFiles.length > 1) {
- createNavFiles();
- }
- }, 100);
- });
- }
-
-
- /!*===============================
- === Execution de l'application
- ================================*!/
- (function(){
-
- initPage(MdFiles[0]);
-
- $('body').on('click', 'button', function(){
- initPage( $(this).data(GenericNames.data_btnFilesNav) );
- });
-
- })();*/
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,4 @@
+//TODO : finir de commenter les methodes
 //TODO : faire la doc de markdocs...
 //TODO : page d'erreur si .md pas trouvé
 //TODO : trier liste filesNav
@@ -6,37 +7,141 @@
 let $ = jQuery,
     sdn = showdown;
 
-
-const   GenericNames = {
-        docBody: 'markdocs-render',
-        docNav: 'markdocs-nav',
-        filesNav: 'markdocs-nav-files',
-        loadRender: 'markdocs-renderLoad',
-        data_btnFilesNav: 'file-name'
+const Defaults = {
+    mdFiles: undefined,
+    showdownOptions: {
+        'tables': true
     },
-        ShowdownOptions = {
-            'tables': true
-        },
-        DefaultOptions = {
-        };
-
-let DocFiles = [];// tableau d'objet qui liste les différents fichiers de doc et leurs infos
+    genericNames: {
+        container_docBody: 'markdocs-render',
+        container_docNav: 'markdocs-nav',
+        container_filesNav: 'markdocs-nav-files',
+        container_loadRender: 'markdocs-renderLoad',
+        data_btnFilesNav: 'file-name'
+    }
+};
 
 
 class Markdocs {
 
     constructor(options){
+        const th = this;
+
         // =*= Propriétés
-        this._options = {};
-        this._options.filesArray = options.mdFiles;
+        th._settings = Markdocs._mergeOptions(options);
+        th._docFiles = [];
 
         // =*= Vérifications
-        if( typeof this._options.filesArray === 'undefined' || !$.isArray(this._options.filesArray) )
+        if( typeof th._settings.mdFiles === 'undefined' || !$.isArray(th._settings.mdFiles) )
             throw new Error(`Il manque l'index mdFiles, mdFiles doit être un tableau...`);
 
         // =*= Execution
-        Markdocs._initPage(this._options.filesArray, this._options.filesArray[0]);
+        th._init();
+
+        // =*= Events
+        $('body').on('click', 'button', function(){
+            th._loadPage(th._settings.mdFiles, $(this).data(th._settings.genericNames.data_btnFilesNav));
+        });
     }
+
+    /**
+     * Chargement de la page avec le fichier par défaut à l'arrivée sur la doc
+     *
+     * @private
+     */
+    _init(){
+        this._loadPage(this._settings.mdFiles, this._settings.mdFiles[0]);
+    }
+
+    /**
+     * Merge les options user/default et retourne l'objet correspondant
+     *
+     * @private
+     */
+    static _mergeOptions(options){
+        return $.extend( true, {}, Defaults, options );
+    }
+
+    /**
+     * Chargement ou rechargement de la page en fonction de la catégorie choisie
+     *
+     * @param arrayFiles
+     * @param filePage
+     * @private
+     */
+    _loadPage(arrayFiles, filePage){
+
+        // -**- Lit le fichier md, le converti en html et envoie son contenu à la vue
+        Markdocs._readMdFile(filePage, data => {
+
+            this._sendToHtml( this._parseMdToHtml(data) );
+
+        });
+
+        // -**- attend la fin du chargement du contenu pour générer la navigation...
+        $(`#${this._settings.genericNames.loadRender}`).ready( () => {
+            setTimeout( () => {
+                // -***-
+                this._createNavPage();
+
+                Markdocs._convertToObject(arrayFiles, this._docFiles);
+
+                if(arrayFiles.length > 1) {
+                    this._createNavFiles();
+                }
+            }, 100);
+        });
+    }
+
+    _sendToHtml(txt){
+        $( `#${this._settings.genericNames.container_docBody}` )
+            .html( `<div id="${this._settings.genericNames.container_loadRender}">${txt}</div>` );
+    }
+
+    _parseMdToHtml(md){
+        let converter = new sdn.Converter();
+
+        // application des options
+        for (let opp in this._settings.showdownOptions){
+            converter.setOption(opp, this._settings.showdownOptions[opp]);
+        }
+
+        return converter.makeHtml(md);
+    }
+
+    _createNavFiles(){
+        const th = this;
+
+        // création du container
+        let template = `<hr><div id="${th._settings.genericNames.container_filesNav}"><p>Catégories disponibles&nbsp;:</p></div>`;
+
+        $(`#${th._settings.genericNames.container_docNav}`).append(template);
+
+        // parcourt du tableau de fichiers
+        th._docFiles.forEach(function(obj){
+            $(`#${th._settings.genericNames.container_filesNav}`)
+                .append(
+                    `<span>
+                        <button data-${th._settings.genericNames.data_btnFilesNav}="${obj['path']}" class="btn__navfiles">${obj['name']}</button>
+                    </span>`);
+        });
+    }
+
+    _createNavPage(){
+        const th = this;
+
+        let titleList = $('h1, h2, h3, h4, h5, h6'),
+            template = `<nav><ul></ul></nav>`;
+
+        $(`#${th._settings.genericNames.container_docNav}`).html(template);
+
+        titleList.each( function(){
+            let classTitle = `title_nav title_${ $(this).get(0).tagName }`;
+
+            $(`#${th._settings.genericNames.container_docNav}`).find('ul').append(`<li><a class="${classTitle}" href="#${ $(this).attr('id') }">${ $(this).html() }</a></li>`)
+        });
+    }
+
 
     static _rmExtension(file){
 
@@ -49,19 +154,13 @@ class Markdocs {
         return resultNameArr.pop();
     }
 
-    static _convertToObject(array){
+    static _convertToObject(array, obj){
 
         for(let i=0; array.length > i; i++){
-            DocFiles[i] = {};
-            DocFiles[i].name = this._rmExtension(array[i]);
-            DocFiles[i].path = array[i];
+            obj[i] = {};
+            obj[i].name = Markdocs._rmExtension(array[i]);
+            obj[i].path = array[i];
         }
-    }
-
-    static _sendToHtml(txt){
-
-        $(`#${GenericNames.docBody}`).html(`<div id="${GenericNames.loadRender}">${txt}</div>`);
-
     }
 
     static _readMdFile(urlFile, action){
@@ -87,199 +186,4 @@ class Markdocs {
             throw new Error('Il y a eu une erreur lors du chargement du fichier...');
         }
     }
-
-    static _parseMdToHtml(md){
-        let converter = new sdn.Converter();
-
-        // application des options
-        for (let opp in ShowdownOptions){
-            converter.setOption(opp, ShowdownOptions[opp]);
-        }
-
-        return converter.makeHtml(md);
-
-    }
-
-    static _createNavFiles(){
-        // création du container
-        let template = `<hr><div id="${GenericNames.filesNav}"><p>Catégories disponibles&nbsp;:</p></div>`;
-
-        $(`#${GenericNames.docNav}`).append(template);
-
-        // parcourt du tableau de fichiers
-        DocFiles.forEach(function(obj){
-            $(`#${GenericNames.filesNav}`)
-                .append(
-                    `<span>
-                        <button data-${GenericNames.data_btnFilesNav}="${obj['path']}" class="btn__navfiles">${obj['name']}</button>
-                    </span>`);
-        });
-    }
-
-    static _createNavPage(){
-        let titleList = $('h1, h2, h3, h4, h5, h6'),
-            template = `<nav><ul></ul></nav>`;
-
-        $(`#${GenericNames.docNav}`).html(template);
-
-        titleList.each( function(){
-            let classTitle = `title_nav title_${ $(this).get(0).tagName }`;
-
-            $(`#${GenericNames.docNav}`).find('ul').append(`<li><a class="${classTitle}" href="#${ $(this).attr('id') }">${ $(this).html() }</a></li>`)
-        });
-    }
-
-    static _initPage(arrayFiles, filePage){
-
-        // -**- Lit le fichier md, le converti en html et envoie son contenu à la vue
-        Markdocs._readMdFile(filePage, data => {
-
-            Markdocs._sendToHtml( Markdocs._parseMdToHtml(data) );
-
-        });
-
-        // -**- attend la fin du chargement du contenu pour générer la navigation...
-        $(`#${GenericNames.loadRender}`).ready( () => {
-            setTimeout( () => {
-                // -***-
-                Markdocs._createNavPage();
-
-                Markdocs._convertToObject(arrayFiles);
-                console.log(arrayFiles);
-
-                if(arrayFiles.length > 1) {
-                    Markdocs._createNavFiles();
-                }
-            }, 100);
-        });
-    }
 }
-
-
-
-
-/*
-
-
- function rmExtension(file){
-
- let arr = file.trim().split('.');
- arr.pop();
-
- let stringPath = arr.join('.'),
- resultNameArr = stringPath.split('/');
-
- return resultNameArr.pop();
- }
-
- function convertToObject(array){
-
- for(let i=0; array.length > i; i++){
- DocFiles[i] = {};
- DocFiles[i].name = rmExtension(array[i]);
- DocFiles[i].path = array[i];
- }
- }
-
- function sendToHtml(txt){
-
- $(`#${GenericNames.docBody}`).html(`<div id="${GenericNames.loadRender}">${txt}</div>`);
-
- }
-
- function readMdFile(urlFile, action){
-
- let reader = new XMLHttpRequest();
-
- reader.onload = function(){
- let data = this.responseText;
-
- if(typeof action === 'function') {
- action(data);
- }
- };
-
- reader.open("GET", urlFile + ((/\?/).test(urlFile) ? "&" : "?") + (new Date()).getTime(), true);
-
- reader.overrideMimeType("text/markdown; charset=UTF-8");
- reader.setRequestHeader("Cache-Control", "no-cache");
-
- if(reader.status == 0 || reader.status == 200) {
- return reader.send();
- }else{
- throw new Error('Il y a eu une erreur lors du chargement du fichier...');
- }
- }
-
- function parseMdToHtml(md){
- let converter = new showdown.Converter();
-
- // application des options
- for (let opp in ShowdownOptions){
- converter.setOption(opp, ShowdownOptions[opp]);
- }
-
- return converter.makeHtml(md);
-
- }
-
- function createNavFiles(){
- // création du container
- let template = `<hr><div id="${GenericNames.filesNav}"><p>Catégories disponibles&nbsp;:</p></div>`;
- $(`#${GenericNames.docNav}`).append(template);
-
- // parcourt du tableau de fichiers
- DocFiles.forEach(function(obj){
- $(`#${GenericNames.filesNav}`)
- .append(
- `<span>
- <button data-${GenericNames.data_btnFilesNav}="${obj['path']}" class="btn__navfiles">${obj['name']}</button>
- </span>`);
- });
-
- }
-
- function createNavPage(){
- let titleList = $('h1, h2, h3, h4, h5, h6'),
- template = `<nav><ul></ul></nav>`;
-
- $(`#${GenericNames.docNav}`).html(template);
-
- titleList.each( function(){
- let classTitle = `title_nav title_${ $(this).get(0).tagName }`;
-
- $(`#${GenericNames.docNav}`).find('ul').append(`<li><a class="${classTitle}" href="#${ $(this).attr('id') }">${ $(this).html() }</a></li>`)
- });
- }
-
- function initPage(filePage){
- readMdFile(filePage, data => {
- sendToHtml(parseMdToHtml(data));
- });
-
- $(`#${GenericNames.loadRender}`).ready( () =>{
- setTimeout( () => {
- createNavPage();
-
- convertToObject(MdFiles);
-
- if(MdFiles.length > 1) {
- createNavFiles();
- }
- }, 100);
- });
- }
-
-
- /!*===============================
- === Execution de l'application
- ================================*!/
- (function(){
-
- initPage(MdFiles[0]);
-
- $('body').on('click', 'button', function(){
- initPage( $(this).data(GenericNames.data_btnFilesNav) );
- });
-
- })();*/
